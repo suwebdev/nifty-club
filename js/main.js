3eec45f002e8c0c20111b53f7600b279ad0af742
Dineen <35275339+dineensch@users.noreply.github.com>
2019-02-07 12:25:35 -0800

    added JS and jQuery 'Like' buttons to index.html page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+////// LIKE BUTTON ////////
 
+// jQUERY
+
+$(function () {
+    $(".like").click(function() {
+        var input = $(this).find('.qty1');
+        input.val(parseInt(input.val())+ 1);
+    });
+});
+
+// JAVASCRIPT
+var i = 0;
+function buttonClick() {
+    document.getElementById('clicks').value = ++i;
+}
